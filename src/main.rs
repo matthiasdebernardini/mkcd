2bc794d457423e74b297b7ef8e90daf16cad5f04
matthiasdebernardini <m.f.debern@protonmail.com>
2023-02-01 14:07:39 -0600

    Fixing logic again

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::{env, fs, path::Path};
 
 fn main() -> Result<()> {
     let mut cwd = env::current_dir()?;
-    let new_dir = env::args().next().context("Could not get argument")?;
-    let path = Path::new(&new_dir);
+    dbg!(cwd.clone());
+    dbg!(env::args().collect::<Vec<String>>());
+    let new_dir = env::args().collect::<Vec<String>>();
+
+    let path = Path::new(&new_dir[1]);
+    dbg!(path.clone());
 
     cwd.extend(path);
 
+    dbg!(cwd.clone());
     fs::create_dir_all(path).context(format!("Could not create directories {}", path.display()))?;
     env::set_current_dir(path).context(format!(
         "Could not change directories to {}",
