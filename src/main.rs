8caa304cec5bc6614bb7be123d96635bec1168c7
matthiasdebernardini <m.f.debern@protonmail.com>
2023-02-01 13:18:15 -0600

    Fixing logic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use anyhow::{Context, Result};
 use std::{env, fs, path::Path};
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let path = env::args().next().context("Could not get argument")?;
     let path = Path::new(&path);
 
+    fs::create_dir_all(path).context(format!("Could not create directories {}", path.display()))?;
     env::set_current_dir(path).context(format!(
         "Could not change directories to {}",
         path.display()
-    ))?;
+    ))
 
-    fs::create_dir_all(path).context(format!("Could not create directories {}", path.display()))
 }
