1843288fdf721eb49e2c8cfadbb3041deb8ad929
matthiasdebernardini <m.f.debern@protonmail.com>
2023-01-31 20:00:33 -0600

    Added anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use std::{env, fs, path::Path};
+use anyhow::{Context, Result};
 
-fn main() {
-    let path = match env::args().next() {
-        Some(p) => p,
-        None => panic!("Cannot make folder without at least a name for it"),
-    };
+fn main() -> anyhow::Result<()> {
+    let path = get_path()?;
+    let path = Path::new(&path);
 
-    let path = Path::new(path.as_str());
+    create_dir(&path)?;
 
-    if let true = path.is_dir() {
-        let path = path.to_str().expect("Path provided must be valid UTF-8");
-        panic!("{} already exists", path)
-    };
+    change_dir(&path)
+}
 
-    match fs::create_dir(path) {
-        Ok(_) => (),
-        Err(e) => panic!("Error creating new folder: {}", e),
-    };
+fn change_dir(path: &Path) -> anyhow::Result<()> {
+    env::set_current_dir(path).context(format!("Could not change directories to {}", path.display()))
+}
 
-    match env::set_current_dir(path) {
-        Ok(_) => (),
-        Err(e) => panic!("Cannot change directory to new path due to {}", e),
-    }
+fn get_path() -> Result<String> {
+    env::args().next().context("Could not get argument")
+}
+
+fn create_dir(path: &Path) -> Result<()> {
+    fs::create_dir_all(path).context(format!("Could not create directories {}", path.display()))
 }
