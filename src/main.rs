fe38baea99512ac3e75dbcd31c86a4454f099205
matthiasdebernardini <m.f.debern@protonmail.com>
2023-02-01 14:19:25 -0600

    Fixing dir logic

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ use std::{env, fs, path::Path};
 fn main() -> Result<()> {
     let mut cwd = env::current_dir()?;
 
-    let new_dir = env::args().collect::<Vec<String>>();
+    let path = env::args().nth(1).context("Could not get argument for directory name")?;
 
-    let path = Path::new(&new_dir[1]);
+    let path = Path::new(&path);
 
     cwd.extend(path);
 
-    fs::create_dir_all(cwd.clone())
+    fs::create_dir_all(&cwd)
         .context(format!("Could not create directories {}", path.display()))?;
 
-    env::set_current_dir(cwd.clone()).context(format!(
+    env::set_current_dir(&cwd).context(format!(
         "Could not change directories to {}",
         path.display()
     ))
