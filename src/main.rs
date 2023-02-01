c2071168144d12855041129734ffc5e336097fc1
matthiasdebernardini <m.f.debern@protonmail.com>
2023-02-01 10:43:40 -0600

    Oxidized functions

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-use std::{env, fs, path::Path};
 use anyhow::{Context, Result};
+use std::{env, fs, path::Path};
 
 fn main() -> anyhow::Result<()> {
-    let path = get_path()?;
+    let path = env::args().next().context("Could not get argument")?;
     let path = Path::new(&path);
 
-    create_dir(&path)?;
+    env::set_current_dir(path).context(format!(
+        "Could not change directories to {}",
+        path.display()
+    ))?;
 
-    change_dir(&path)
-}
-
-fn change_dir(path: &Path) -> anyhow::Result<()> {
-    env::set_current_dir(path).context(format!("Could not change directories to {}", path.display()))
-}
-
-fn get_path() -> Result<String> {
-    env::args().next().context("Could not get argument")
-}
-
-fn create_dir(path: &Path) -> Result<()> {
     fs::create_dir_all(path).context(format!("Could not create directories {}", path.display()))
 }
